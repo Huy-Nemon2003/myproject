952c4e29d94c1ddc457f610a9e4e5422627a65da
Huy <magettinh@gmail.com>
2025-05-29 23:39:50 +0700

    hoàn thành demo trò chơi hangman

--- src/Keybroad.tsx
+++ src/Keybroad.tsx
@@ -1,7 +1,67 @@
-export function Keybroad() {
+import styles from "./Keyboard.module.css";
+
+const KEYS = [
+  "a",
+  "b",
+  "c",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+type KeyboardProps = {
+  disabled?: boolean;
+  activeLetters: string[];
+  inactiveLetters: string[];
+  addGuessedLetter: (letter: string) => void;
+};
+export function Keybroad({
+  activeLetters,
+  disabled = false,
+  inactiveLetters,
+  addGuessedLetter,
+}: KeyboardProps) {
   return (
-    <>
-      <h1>hi</h1>
-    </>
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(auto-fit, minmax(75px, 1fr))",
+        gap: ".5rem",
+      }}
+    >
+      {KEYS.map((key) => {
+        const isActive = activeLetters.includes(key);
+        const isInactive = inactiveLetters.includes(key);
+        return (
+          <button
+            onClick={() => addGuessedLetter(key)}
+            className={`${styles.btn} ${isActive ? styles.active : ""}
+             ${isInactive ? styles.inactive : ""}`}
+            disabled={isInactive || isActive || disabled}
+            key={key}
+          >
+            {key}
+          </button>
+        );
+      })}
+    </div>
   );
 }
